79a92bec94648e28b952ea622567bcf333e27734
João Almeida <joao.marques.almeida@tecnico.ulisboa.pt>
2020-03-08 12:58:15 +0000

    lab done

--- LabMS1920-1/carro.m
+++ LabMS1920-1/carro.m
@@ -1,7 +1,41 @@
 massa = 30;
 beta = 5;
-Vo = timeseries(3);
-y0 = 5;
-stop_time = 100;
+Vo_ = 3;
+Yo_ = 5;
+Yo = timeseries(5);
+stop_time = 1;
 
-sim('movimento')
+clf;
+figure(1); grid on; hold on;
+figure(2); grid on; hold on;
+
+v_legend_1=['Vo = ' num2str(-Vo_)];
+v_legend_2=['Vo = ' num2str(Vo_)];
+y_legend_1=['Vo = ' num2str(-Yo_)];
+y_legend_2=['Vo = ' num2str(Yo_)];
+
+    
+for Vo = [timeseries(-Vo_), timeseries(Vo_)]
+    sim_out = sim('movimento');
+    figure(1)
+    plot(sim_out.tout, sim_out.velocity);
+    figure(2)
+    plot(sim_out.position.Time, sim_out.position.Data);
+end
+
+figure(1);
+legend(v_legend_1, v_legend_2, 'Location', 'best'); hold off;
+title('Velocity');xlabel('t [s]');ylabel('v(t) [m/s]');
+figure(2);
+legend(y_legend_1, y_legend_2, 'Location', 'best'); hold off;
+title('Position');xlabel('t [s]');ylabel('y(t) [m]')
+
+%Porque é que a v(y), não é linear
+figure(3);
+plot(sim_out.position.Data, sim_out.velocity);
+title('velocity(position)');xlabel('y(t) [m]');ylabel('v(t) [m/s]')
+grid on;
+%https://www.mathworks.com/help/deeplearning/ref/regression.html
+[r,m,b] = regression(sim_out.position.Data', sim_out.velocity');
+legend_vy = ['v = ' num2str(m) '*y + ' num2str(b)];
+legend(legend_vy, 'Location', 'best'); hold off;
