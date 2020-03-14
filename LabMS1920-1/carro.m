7705fff9ae1f3de4fbb78ebc2c94b39788d93930
João Almeida <joao.marques.almeida@tecnico.ulisboa.pt>
2020-03-14 13:06:26 +0000

    ainda não fiz variável com dimensão dos eixos

--- LabMS1920-1/carro.m
+++ LabMS1920-1/carro.m
@@ -5,7 +5,7 @@ massa_ = [30 25 30];
 beta_ = [5 5 4];
 Vo_ = [-3, 3];
 Yo_ = 5;
-Yo = timeseries(5);
+Yo = timeseries(Yo_);
 stop_time = 25;
 
 figure(1);clf; grid on; hold on;
@@ -27,6 +27,7 @@ for i = 1:3
         Vo = timeseries(Vo_(j));
         sim_out = sim('movimento');
         figure(1)
+        % i+(j-1)*3 -- increase 1:6 trought i and j
         plotHandlesV(i+(j-1)*3) = plot(sim_out.tout, sim_out.velocity);
         plotLabelsV{i+(j-1)*3}=['Vo = ' num2str(Vo_(j)) 'm/s; massa = ' num2str(massa) 'Kg; coef. atrito = ' num2str(beta) 'Nm/s'];
         figure(2)
@@ -35,8 +36,25 @@ for i = 1:3
     end
 end
 
+%write plot legend
 figure(1);
-lgdv = legend(plotHandlesV, plotLabelsV, 'Location', 'best');
+lgdv = legend(plotHandlesV, plotLabelsV, 'Location', 'northeast');
 figure(2);
 lgdy = legend(plotHandlesY, plotLabelsY, 'Location', 'best');
 lgdy.NumColumns = 2;
+
+
+%draw elips in steady points
+figure(1);
+elpsv1 = annotation('ellipse',[0.1 .9 .05 .05]);
+tav1 = annotation('textarrow', [0.25 0.15], [0.83 0.91]);
+tav1.String = 'Vo';
+elpsv2 = annotation('ellipse',[0.1 .09 .05 .05]);
+tav2 = annotation('textarrow', [0.25 0.15], [0.19 0.13]);
+tav2.String = '-Vo';
+
+
+figure(2);
+elpsy = annotation('ellipse',[0.1 .49 .05 .05]);
+tay = annotation('textarrow', [0.25 0.15], [0.42 0.5]);
+tay.String = 'yo';
