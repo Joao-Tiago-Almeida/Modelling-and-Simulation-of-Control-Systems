68344c3a711b068890940f17985d70319d9e819b
João Almeida <joao.marques.almeida@tecnico.ulisboa.pt>
2020-05-10 16:35:12 +0100

    change question 13 function name

---
+++ Dinâmica de um metrónomo básico/plotSS.m
@@ -0,0 +1,16 @@
+function f = plotSS(y,t)
+    % plot Steady-Space: y2(y1)
+    % creats new figure if nargin>1
+
+    f = 'none'; % default: no figure 
+    
+    if nargin > 1
+        f = figure(); clf; hold on; grid on;
+        title(t,'FontName','Arial','FontSize',14,'interpreter','latex');
+        xlabel('$\theta$ [rad]','interpreter', 'latex','FontName','Arial','FontSize',13);
+        ylabel('$\dot{\theta}$ [rad/s]','interpreter','latex','FontName','Arial','FontSize',13);
+    end
+    plot(y.simout.Data(:,1), y.simout.Data(:,2));
+    
+
+end
